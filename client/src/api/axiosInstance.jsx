d9c77f7051064fa0f1360c3d4a3b7cce67f70743
quang huy <huyquang1282000@gmail.com>
2025-05-15 23:36:41 +0700

    change file

--- client/src/api/axiosInstance.jsx
+++ client/src/api/axiosInstance.jsx
@@ -1,7 +1,8 @@
 import { refreshToken } from '@/redux/authSlice';
 import axios from 'axios';
 
-export const BASE_URL = 'https://restaurant-management-o74o.vercel.app/api/v1';
+export const BASE_URL =
+    'https://restaurant-management-o74o-mjpxyku51.vercel.app/api/v1';
 
 export const axiosInstancePublic = axios.create({
     baseURL: BASE_URL,
